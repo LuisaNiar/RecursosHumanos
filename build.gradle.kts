6791e835f66ddfcd5123f5bc485dec4d13ed3e1d
LuisaNiar <luisaniar@unisabana.edu.co>
2023-10-26 22:59:28 -0500

    Actualización H2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "2.7.15"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    jacoco
     id("org.sonarqube") version "4.4.1.3373"
+    jacoco
 }
 
 group = "co.empresa"
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("mysql:mysql-connector-java:8.0.32")
+    runtimeOnly("com.h2database:h2")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
     implementation("org.springframework.boot:spring-boot-starter:3.1.2")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
