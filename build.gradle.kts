37fd5ee8b674114a8e32ad04f2956650b82e8500
LuisaNiar <luferniar28@gmail.com>
2023-10-27 21:55:38 -0500

    Mutation Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "2.7.15"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     id("org.sonarqube") version "4.4.1.3373"
+    id("info.solidsoft.pitest") version "1.15.0"
     jacoco
 }
 
@@ -20,8 +21,10 @@ repositories {
 
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.pitest:pitest:1.15.0")
     implementation("mysql:mysql-connector-java:8.0.32")
     runtimeOnly("com.h2database:h2")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
@@ -29,12 +32,17 @@ dependencies {
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("io.springfox:springfox-swagger2:3.0.0")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.28")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.1.4")
+
     annotationProcessor("org.projectlombok:lombok:1.18.28")
-    testCompileOnly("org.projectlombok:lombok:1.18.28")
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    runtimeOnly("com.h2database:h2")
+    pitest("org.pitest:pitest-junit5-plugin:1.2.0")
+
     testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.springframework.boot:spring-boot-starter-security:3.1.4")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.10")
+    testCompileOnly("org.projectlombok:lombok:1.18.28")
 }
 
 tasks.withType<Test> {
@@ -76,6 +84,9 @@ sonarqube {
         property("sonar.recursoshumanos", "yms-gate")
 
     }
-
 }
 
+pitest {
+    junit5PluginVersion.set("1.0.0")
+    outputFormats.set(setOf("XML", "HTML"))
+}
